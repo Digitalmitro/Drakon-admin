ad78ac13d6449f8eea81f9c867e42e72b35a1b2c
soumendigitalmitro <soumen.digitalmitro@gmail.com>
2025-06-06 18:18:51 +0530

    new changes

--- src/views/CreateEmployeeProfile/CreateEmployeeProfile.jsx
+++ src/views/CreateEmployeeProfile/CreateEmployeeProfile.jsx
@@ -1,12 +1,13 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import { CCard, CCardHeader, CCardBody,CButton,CForm,CRow,CCol,CFormLabel, CFormInput } from '@coreui/react';
+import { CCard, CCardHeader, CCardBody, CButton, CForm, CRow, CCol, CFormLabel, CFormInput, CFormSelect } from '@coreui/react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { PlusOutlined } from '@ant-design/icons';
 import { Image, Upload } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import Select from 'react-select';
 
 const CreateEmployeeProfile = () => {
   const navigate = useNavigate();
@@ -28,8 +29,10 @@ const CreateEmployeeProfile = () => {
     title: '',
     description: '',
     category: '',
-    price: '',
-    stock: '',
+    price: 0,
+    stock: 0,
+    size: [],
+    weight: 0,
   });
 
   const handlePreview = async (file) => {
@@ -49,14 +52,16 @@ const CreateEmployeeProfile = () => {
         image: fileList.map((file) => file.thumbUrl), // Adjust this according to how your server expects the image data
         ...formData,
       });
-      console.log(response.data); // Log the response from the server
+      console.log(formData); // Log the response from the server
       toast.success('Product created successfully');
       setFormData({
         title: '',
         description: '',
         category: '',
-        price: '',
-        stock: '',
+        price: 0,
+        stock: 0,
+        size: [],
+        weight: 0
       });
       setFileList([]);
     } catch (error) {
@@ -91,114 +96,200 @@ const CreateEmployeeProfile = () => {
   //   'Home Appliances',
   //   'Sports',
   // ];
+  const sizeOptions = ["XXL", "XL", "L", "M", "S", "XS"].map((size) => ({
+    value: size,
+    label: size,
+  }));
 
+  const defaultSizeValues = sizeOptions.filter(option =>
+    [].includes(option.value)
+  );
   return (
     <>
       <ToastContainer />
       <CCard className="shadow-sm p-3">
-      {/* Card Header */}
-      <CCardHeader className="d-flex justify-content-between bg-primary text-white">
-        <h4 className="fw-bold">Add Product</h4>
-        <CButton color="info" className="text-white" onClick={() => navigate("/product-list")}>
-          Product List
-        </CButton>
-      </CCardHeader>
+        {/* Card Header */}
+        <CCardHeader className="d-flex justify-content-between bg-primary text-white">
+          <h4 className="fw-bold">Add Product</h4>
+          <CButton color="info" className="text-white" onClick={() => navigate("/product-list")}>
+            Product List
+          </CButton>
+        </CCardHeader>
 
-      {/* Card Body */}
-      <CCardBody >
-        <CForm onSubmit={handleSubmit}>
-          <CRow className="mb-3">
-            {/* Product Title */}
-            <CCol md={6}>
-              <CFormLabel>Product Title</CFormLabel>
-              <CFormInput
-                type="text"
-                placeholder="Enter product title"
-                required
-                value={formData.title}
-                onChange={(e) => setFormData({ ...formData, title: e.target.value })}
-              />
-            </CCol>
+        {/* Card Body */}
+        <CCardBody >
+          <CForm onSubmit={handleSubmit}>
+            <CRow className="mb-4">
+              {/* Product Title */}
+              <CCol md={6}>
+                <CFormLabel>Product Title</CFormLabel>
+                <CFormInput
+                  type="text"
+                  placeholder="Enter product title"
+                  required
+                  value={formData.title}
+                  onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+                />
+              </CCol>
 
-            {/* Product Category */}
-            <CCol md={6}>
-              <CFormLabel>Category</CFormLabel>
-              <CFormInput
-                type="text"
-                placeholder="Enter category"
-                required
-                value={formData.category}
-                onChange={(e) => setFormData({ ...formData, category: e.target.value })}
-              />
-            </CCol>
-          </CRow>
+              {/* Product Category */}
+              <CCol md={6}>
+                <CFormLabel>Category</CFormLabel>
+                <CFormSelect size="sm" className="mb-3" aria-label="Small select example" value={formData.category} required placeholder="Enter category" onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
+                  <option>--select-category--</option>
+                  <option value="Sunglasses">Sunglasses</option>
+                  <option value="Accessories">Accessories</option>
+                  <option value="Apparel">Apparel</option>
+                  <option value="Batting Gloves">Batting Gloves</option>
+                  <option value="Equipment">Equipment</option>
+                </CFormSelect>
 
-          {/* Product Description */}
-          <CRow className="mb-3">
-            <CCol md={12}>
-              <CFormLabel>Description</CFormLabel>
-              <CFormInput
-                type="text"
-                placeholder="Enter product description"
-                required
-                value={formData.description}
-                onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-              />
-            </CCol>
-          </CRow>
+              </CCol>
+            </CRow>
 
-          <CRow className="mb-3">
-            {/* Product Price */}
-            <CCol md={6}>
-              <CFormLabel>Price</CFormLabel>
-              <CFormInput
-                type="number"
-                placeholder="Enter price"
-                required
-                value={formData.price}
-                onChange={(e) => setFormData({ ...formData, price: e.target.value })}
-              />
-            </CCol>
+            {/* Product Description */}
+            <CRow className="mb-4">
+              <CCol md={12}>
+                <CFormLabel>Description</CFormLabel>
+                <CFormInput
+                  type="text"
+                  placeholder="Enter product description"
+                  required
+                  value={formData.description}
+                  onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+                />
+              </CCol>
+            </CRow>
+            <CRow className="mb-4">
+              <CCol md={6}>
+                <CFormLabel>Size</CFormLabel>
+                <Select
+                  defaultValue={defaultSizeValues}
+                  isMulti
+                  name="sizes"
+                  options={sizeOptions}
+                  styles={{
 
-            {/* Product Stock */}
-            <CCol md={6}>
-              <CFormLabel>Stock</CFormLabel>
-              <CFormInput
-                type="number"
-                placeholder="Enter stock quantity"
-                required
-                value={formData.stock}
-                onChange={(e) => setFormData({ ...formData, stock: e.target.value })}
-              />
-            </CCol>
-          </CRow>
+                    control: (base) => ({
+                      ...base,
+                      backgroundColor: '#212631',
+                      borderColor: '#323a49',
+                      boxShadow: 'none',
+                      '&:hover': {
+                        borderColor: '#323a49',
+                      },
+                    }),
+                    menu: (base) => ({
+                      ...base,
+                      backgroundColor: '#323a49',
+                      color: '#fff',
 
-          {/* Image Upload */}
-          <CRow className="mb-3">
-            <CCol md={12}>
-              <CFormLabel>Product Image</CFormLabel>
-              <Upload
-                action="https://your-api/upload"
-                listType="picture-card"
-                fileList={fileList}
-                onChange={handleChange}
-              >
-                {fileList.length >= 5 ? null : <CButton color="secondary">Upload</CButton>}
-              </Upload>
-            </CCol>
-          </CRow>
+                    }),
+                    option: (base, state) => ({
+                      ...base,
+                      backgroundColor: state.isSelected ? '#323a49' : '#212631',
+                      color: state.isSelected ? '#fff' : '#aaa',
+                      '&:hover': {
+                        backgroundColor: '#323a49',
+                        color: '#fff',
+                      },
+                    }),
+                    multiValue: (base) => ({
+                      ...base,
+                      backgroundColor: '#5e5cd0',
+                      color: '#fff',
 
-          {/* Buttons */}
-          <CRow className="mt-4">
-            <CCol className="text-center">
-              <CButton type="submit" color="success">
-                Submit
-              </CButton>
-            </CCol>
-          </CRow>
-        </CForm>
-      </CCardBody>
-    </CCard>
+                    }),
+                    multiValueLabel: (base) => ({
+                      ...base,
+                      color: '#fff',
+                    }),
+                    multiValueRemove: (base) => ({
+                      ...base,
+                      color: '#fff',
+                      ':hover': {
+                        backgroundColor: 'transparent',
+                        color: '#aaa',
+                      },
+                    }),
+                  }}
+                  onChange={(selectedOptions) => {
+                    setFormData({ ...formData, size: selectedOptions.map((option) => option.value) })
+                  }
+                  }
+                  placeholder="Select sizes"
+                  className="basic-multi-select "
+                  classNamePrefix="select"
+                />
+
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel>Weight</CFormLabel>
+                <CFormInput
+                  type="number"
+                  placeholder="Enter stock quantity"
+                  required
+                  value={formData.weight}
+                  onChange={(e) => setFormData({ ...formData, weight: e.target.value })}
+                />
+              </CCol>
+            </CRow>
+
+            <CRow className="mb-3">
+              {/* Product Price */}
+              <CCol md={6}>
+                <CFormLabel>Price</CFormLabel>
+                <CFormInput
+                  type="number"
+                  placeholder="Enter price"
+                  required
+                  value={formData.price}
+                  onChange={(e) => setFormData({ ...formData, price: e.target.value })}
+                />
+              </CCol>
+
+              {/* Product Stock */}
+              <CCol md={6}>
+                <CFormLabel>Stock</CFormLabel>
+                <CFormInput
+                  type="number"
+                  placeholder="Enter stock quantity"
+                  required
+                  value={formData.stock}
+                  onChange={(e) => setFormData({ ...formData, stock: e.target.value })}
+                />
+              </CCol>
+            </CRow>
+
+            {/* Image Upload */}
+            <CRow className="mb-3">
+              <CCol md={6}>
+                <CFormLabel>Product Image</CFormLabel>
+                <Upload
+                  action="https://your-api/upload"
+                  listType="picture-card"
+                  fileList={fileList}
+                  onChange={handleChange}
+                >
+                  {fileList.length >= 5 ? null : <CButton color="secondary">Upload</CButton>}
+                </Upload>
+              </CCol>
+
+            </CRow>
+
+            {/* Buttons */}
+
+            {/* Buttons */}
+            <CRow className="mt-4">
+              <CCol className="text-center">
+                <CButton type="submit" color="success">
+                  Submit
+                </CButton>
+              </CCol>
+            </CRow>
+          </CForm>
+        </CCardBody>
+      </CCard>
     </>
   );
 };
