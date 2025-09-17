22c2a1a30901cc2e69441e344f6f1d256b469829
Twinkle0901 <twinkle.digitalmitro@gmail.com>
2025-09-17 11:35:34 +0530

    added

--- vite.config.mjs
+++ vite.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [react(),tailwindcss()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: [
         {
